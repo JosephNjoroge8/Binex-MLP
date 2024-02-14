87dd56e4fa8a85ed536799251c6778fb46b5b842
Joseph <Joseph>
2024-02-14 13:37:58 +0300

    my newcommit term

--- .Binex/app.py
+++ .Binex/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
@@ -8,52 +8,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
-    # Load the loan eligibility dataset
-    data = pd.read_csv('loan_dataset.csv')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-    # Preprocess the data
-    X = data.drop('Loan_Status', axis=1)
-    y = data['Loan_Status']
+@app.route('/form')
+def form():
+    return render_template('userdataform.html')
 
-    # Train the random forest classifier
-    clf = RandomForestClassifier()
-    clf.fit(X, y)
-
-    # Get user input from the HTML form
-    gender = request.form.get('gender')
-    married = request.form.get('married')
-    dependents = request.form.get('dependents')
-    education = request.form.get('education')
-    self_employed = request.form.get('self_employed')
-    applicant_income = float(request.form.get('applicant_income'))
-    coapplicant_income = float(request.form.get('coapplicant_income'))
-    loan_amount = float(request.form.get('loan_amount'))
-    loan_term = float(request.form.get('loan_term'))
-    credit_history = float(request.form.get('credit_history'))
-    property_area = request.form.get('property_area')
-
-    # Create a new dataframe with the user input
-    user_input = pd.DataFrame({
-        'Gender': [gender],
-        'Married': [married],
-        'Dependents': [dependents],
-        'Education': [education],
-        'Self_Employed': [self_employed],
-        'ApplicantIncome': [applicant_income],
-        'CoapplicantIncome': [coapplicant_income],
-        'LoanAmount': [loan_amount],
-        'Loan_Amount_Term': [loan_term],
-        'Credit_History': [credit_history],
-        'Property_Area': [property_area]
-    })
-
-    # Make predictions on the user input
-    prediction = clf.predict(user_input)
-
-    # Return the prediction result to the HTML page
-    return render_template('index.html', prediction=prediction[0])
+#@app.route('/predict', methods=['POST', 'GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
